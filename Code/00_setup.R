99f143cb1fd9a5353a8f946ad2e7a9fa2f59ab9c
Léon Kia Faro <leonkiafaro@gmail.com>
2025-05-17 21:08:11 +0200

    Update config to 3 dims and adjust baseline

--- Code/00_setup.R
+++ Code/00_setup.R
@@ -10,13 +10,10 @@ EPS <- 1e-10
 safe_logpdf <- function(val, eps = EPS) log(pmax(val, eps))
 safe_cdf    <- function(val, eps = EPS) pmax(eps, pmin(1 - eps, val))
 
-config4 <- list(
-  list(distr = "norm",    parm = NULL),
-  list(distr = "t",       parm = function(d) list(df = 3 + 0.5 * d$X1)),
-  list(distr = "laplace", parm = function(d) list(m = 0.3 * d$X2,
-                                                  s = clip(1 + 0.1 * d$X2))),
-  list(distr = "logis",   parm = function(d) list(location = 0.2 * d$X3,
-                                                  scale = clip(1 + 0.05 * d$X3)))
+config3 <- list(
+  list(distr = "norm",  parm = NULL),
+  list(distr = "exp",   parm = function(d) list(rate = exp(d$X1))),
+  list(distr = "gamma", parm = function(d) list(shape = d$X2, rate = 1))
 )
-config <- config4
+config <- config3
 K <- length(config)
--- Code/03_param_baseline.R
+++ Code/03_param_baseline.R
@@ -1,22 +1,20 @@
 source("02_generate_data.R")
 
 nll_funs <- list(
-  function(p,xs,Xprev) -sum(dnorm(xs, mean=p[1], sd=exp(p[2]), log=TRUE)),
-  function(p,xs,Xprev) -sum(dt(xs, df=p[1]+p[2]*Xprev[,1], log=TRUE)),
-  function(p,xs,Xprev) -sum(dlaplace(xs, m=p[1]*Xprev[,2], s=exp(p[2]*Xprev[,2]), log=TRUE)),
-  function(p,xs,Xprev) -sum(dlogis(xs, location=p[1]*Xprev[,3], scale=exp(p[2]*Xprev[,3]), log=TRUE))
+  function(p,xs,Xprev) -sum(dnorm(xs, mean = p[1], sd = exp(p[2]), log = TRUE)),
+  function(p,xs,Xprev) -sum(dexp(xs, rate = exp(p[1] + p[2] * Xprev[,1]), log = TRUE)),
+  function(p,xs,Xprev) -sum(dgamma(xs, shape = exp(p[1]) * Xprev[,2], rate = exp(p[2]), log = TRUE))
 )
-init_vals <- list(c(0,0), c(3,0.5), c(0.3,0.1), c(0.2,0.05))
+init_vals <- list(c(0,0), c(0,0), c(0,0))
 param_est <- vector("list", K)
 for (k in seq_len(K)) {
   xs    <- X_pi_train[,k]
   Xprev <- if (k>1) X_pi_train[,1:(k-1),drop=FALSE] else NULL
   fit   <- optim(init_vals[[k]], nll_funs[[k]], xs=xs, Xprev=Xprev)$par
   param_est[[k]] <- switch(k,
-    list(mean=fit[1], sd=exp(fit[2])),
-    list(a=fit[1], b=fit[2]),
-    list(c=fit[1], e=fit[2]),
-    list(g=fit[1], h=fit[2])
+    list(mean = fit[1], sd = exp(fit[2])),
+    list(a = fit[1], b = fit[2]),
+    list(a = fit[1], b = fit[2])
   )
 }
 true_ll_mat_test  <- sapply(seq_len(K), function(k)
@@ -27,10 +25,9 @@ param_ll_mat_test <- sapply(seq_len(K), function(k) {
   Xprev <- if(k>1) X_pi_test[,1:(k-1),drop=FALSE] else NULL
   p     <- param_est[[k]]
   switch(k,
-    dnorm(xs, mean=p$mean, sd=p$sd, log=TRUE),
-    dt(xs, df=p$a + p$b * Xprev[,1], log=TRUE),
-    dlaplace(xs, m=p$c * Xprev[,2], s=exp(p$e * Xprev[,2]), log=TRUE),
-    dlogis(xs, location=p$g * Xprev[,3], scale=exp(p$h * Xprev[,3]), log=TRUE)
+    dnorm(xs, mean = p$mean, sd = p$sd, log = TRUE),
+    dexp(xs, rate = exp(p$a + p$b * Xprev[,1]), log = TRUE),
+    dgamma(xs, shape = exp(p$a) * Xprev[,2], rate = exp(p$b), log = TRUE)
   )
 })
 ll_delta_df_test <- data.frame(
