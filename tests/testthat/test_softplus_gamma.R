0019258ec44ae7f25247963e92cd3fe7352c937c
Léon Kia Faro <leonkiafaro@gmail.com>
2025-05-22 22:53:13 +0200

    Extend param baseline to multiple families

---
+++ tests/testthat/test_softplus_gamma.R
@@ -0,0 +1,36 @@
+library(testthat)
+source("../../00_setup.R", chdir = TRUE)
+source("../../01_transport_utils.R", chdir = TRUE)
+source("../../03_param_baseline.R", chdir = TRUE)
+
+# Softplus positivity
+vals <- seq(-100, 100, length.out = 201)
+
+test_that("softplus positive", {
+  expect_true(all(softplus(vals) > 0))
+})
+
+# Synthetic gamma fit
+set.seed(1)
+N <- 10000
+x_prev <- rnorm(N)
+true_theta <- c(0.5, -0.2, -0.1, 0.3)
+shape <- softplus(true_theta[1] + true_theta[2] * x_prev)
+rate  <- softplus(true_theta[3] + true_theta[4] * x_prev)
+xs <- rgamma(N, shape = shape, rate = rate)
+
+nll <- make_nll("gamma", x_prev, xs)
+fit <- safe_optim(rep(0, 4), nll)
+
+shape_hat <- softplus(fit$par[1] + fit$par[2] * x_prev)
+rate_hat  <- softplus(fit$par[3] + fit$par[4] * x_prev)
+rel <- c(mean(abs(shape_hat - shape) / shape),
+         mean(abs(rate_hat - rate) / rate))
+
+test_that("gamma parameters recovered", {
+  expect_true(all(rel < 0.01))
+})
+
+test_that("nll monotonicity", {
+  expect_lte(nll(fit$par), nll(rep(0, 4)))
+})
