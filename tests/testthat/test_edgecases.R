c8ba45dc6e248ec8a55f2b2a3de6d0f58c331ab9
Léon Kia Faro <leonkiafaro@gmail.com>
2025-05-23 18:24:08 +0200

    Refactor config handling

--- tests/testthat/test_edgecases.R
+++ tests/testthat/test_edgecases.R
@@ -1,4 +1,9 @@
 library(testthat)
+config <- list(
+  list(distr = "norm", parm = NULL),
+  list(distr = "exp",  parm = function(d) list(rate = softplus(d$X1))),
+  list(distr = "gamma", parm  = function(d) list(shape = softplus(d$X2), rate = softplus(d$X1)))
+)
 source("../../00_setup.R", chdir = TRUE)
 
 # Extreme X_k values should not produce NaN log-densities
