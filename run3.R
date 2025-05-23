2caf790c7dbbd62d52a64e1c61a94f3f56ebd6e3
Léon Kia Faro <leonkiafaro@gmail.com>
2025-05-23 18:38:15 +0200

    Update config in run3

--- run3.R
+++ run3.R
@@ -2,8 +2,7 @@
 config <- list(
   list(distr = "norm", parm = NULL),
   list(distr = "exp",  parm = function(d) list(rate = softplus(d$X1))),
-  list(distr = "gamma", parm  = function(d) list(shape = softplus(d$X2), rate = softplus(d$X1))),
-  list(distr = "beta",  parm  = function(d) list(shape1 = softplus(d$X1) + 2, shape2 = softplus(d$X3) + 1))
+  list(distr = "beta", parm  = function(d) list(shape1 = softplus(d$X2), shape2 = 1))
 )
 
 N <- 50
