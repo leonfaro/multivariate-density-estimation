4ead14ce44fb5f3556910cc27aae803bad162c9d
Léon Kia Faro <leonkiafaro@gmail.com>
2025-05-27 15:05:10 +0200

    Implementierung gemeinsamer MLE

---
+++ tests/testthat/test_joint_mle.R
@@ -0,0 +1,13 @@
+library(testthat)
+source("../../03_joint_mle.R", chdir = TRUE)
+
+config <- list(
+  list(distr = "norm", parm = NULL),
+  list(distr = "exp",  parm = function(d) list(rate = d$X1))
+)
+
+parse_param_spec(config, dist_registry)
+
+test_that("param names unique", {
+  expect_true(length(unique(param_names_global)) == length(param_names_global))
+})
