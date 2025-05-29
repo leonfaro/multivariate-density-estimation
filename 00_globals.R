3c8094012d212f746eb6ea72e1c699880050d105
Léon Kia Faro <leonkiafaro@gmail.com>
2025-05-29 23:30:16 +0200

    Update 00_globals.R

--- 00_globals.R
+++ 00_globals.R
@@ -1,13 +1,6 @@
 # Global configuration for TTM experiments
 # Notation follows Theory.md
 
-# Default configuration for component distributions
-config <- list(
-  list(distr = "norm", parm = NULL),
-  list(distr = "exp",  parm = function(d) list(rate = d$X1)),
-  list(distr = "beta", parm = function(d) list(shape1 = d$X2, shape2 = 1)),
-  list(distr = "gamma", parm = function(d) list(shape = d$X3, scale = 1))
-)
 
 #' Initialize global parameters
 #'
