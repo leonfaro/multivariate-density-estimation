a0530639840a81d4237972252d15ccaec9ea5a5c
Léon Kia Faro <leonkiafaro@gmail.com>
2025-05-29 21:06:17 +0200

    Implementiere Script 6 zur Modellbewertung
    Fuegt evaluate_all() entsprechend dem Pseudocode aus roadmap.md hinzu und testet es mit testthat.

---
+++ tests/testthat/test_evaluation.R
@@ -0,0 +1,27 @@
+source("../../00_globals.R")
+source("../../01_data_generation.R")
+source("../../models/ttm_model.R")
+source("../../models/true_model.R")
+source("../../04_evaluation.R")
+
+set.seed(123)
+G <- setup_global()
+X <- gen_samples(G)
+N_tr <- floor(G$split_ratio * G$N)
+X_tr <- X[seq_len(N_tr), ]
+X_te <- X[(N_tr + 1):nrow(X), ]
+
+M_TTM <- fit_TTM(X_tr, X_te, G$H_grid[1])
+M_TRUE <- fit_TRUE(X_tr, X_te, G$config)
+
+models <- setNames(list(M_TTM, M_TRUE), c("TTM", "TRUE"))
+
+
+test_that("evaluate_all returns sorted table", {
+  P <- evaluate_all(X_te, models)
+  expect_s3_class(P, "data.frame")
+  expect_equal(colnames(P), c("model_id", "neg_logL"))
+  expect_equal(nrow(P), length(models))
+  expect_true(all(is.finite(P$neg_logL)))
+  expect_true(all(diff(P$neg_logL) >= 0))
+})
