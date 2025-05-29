39b7e0961a9f6faaf01c6886d1b4f670efe546a2
Léon Kia Faro <leonkiafaro@gmail.com>
2025-05-29 19:12:06 +0200

    Implementiert Stichprobengenerierung

---
+++ tests/testthat/test_data_generation.R
@@ -0,0 +1,12 @@
+source("01_data_generation.R")
+
+# Basic functionality of gen_samples
+
+test_that("gen_samples draws matrix of correct dimension", {
+  G <- setup_global()
+  X <- gen_samples(G)
+  expect_true(is.matrix(X))
+  expect_equal(dim(X), c(G$N, length(G$config)))
+  expect_false(anyNA(X))
+})
+
