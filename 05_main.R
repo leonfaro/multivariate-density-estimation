3d837a93d2cc318b9e99f0fb3816402caf121138
Léon Kia Faro <leonkiafaro@gmail.com>
2025-06-04 08:55:47 +0200

    Update 05_main.R

--- 05_main.R
+++ 05_main.R
@@ -14,7 +14,7 @@ source("02_split.R")
 source("models/true_model.R")
 source("04_evaluation.R")
 
-N <- 500
+N <- 100
 config <- list(
   list(distr = "norm", parm = NULL),
   list(distr = "exp",  parm = function(d) list(rate = d$X1)),
