1ea85137468b0928c7f6931db4be8edb47e31fd9
Léon Kia Faro <leonkiafaro@gmail.com>
2025-06-07 14:53:51 +0200

    Verwende coalesce statt replace_na

--- 04_evaluation.R
+++ 04_evaluation.R
@@ -84,8 +84,8 @@ combine_logL_tables <- function(tab_normal, tab_perm,
 
   tab_all <- left_join(tab_norm, tab_perm, by = c("dim", "distr"))
   tab_all <- tab_all %>%
-    mutate(across(where(is.numeric), replace_na, 0)) %>%
-    mutate(distr = replace_na(distr, "SUM"))
+    mutate(across(where(is.numeric), ~ coalesce(.x, 0))) %>%
+    mutate(distr = coalesce(distr, "SUM"))
 
   tab_all <- tab_all %>%
     add_row(
