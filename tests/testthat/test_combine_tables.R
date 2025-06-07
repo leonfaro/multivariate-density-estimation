7259bde049d1204add7b0e54eb77d7b2a6fc0a1a
Léon Kia Faro <leonkiafaro@gmail.com>
2025-06-07 23:24:34 +0200

    Laufzeitmessung angepasst

--- tests/testthat/test_combine_tables.R
+++ tests/testthat/test_combine_tables.R
@@ -14,20 +14,25 @@ G$n <- 10
 X <- gen_samples(G)
 S <- train_test_split(X, G$split_ratio, G$seed)
 
-M_TRUE <- fit_TRUE(S$X_tr, S$X_te, G$config)
-M_TRTF <- fit_TRTF(S$X_tr, S$X_te, G$config)
-M_KS   <- fit_KS(S$X_tr, S$X_te, G$config)
-M_TTM  <- fit_TTM(S$X_tr, S$X_te)
+t_true <- system.time({
+  M_TRUE <- fit_TRUE(S$X_tr, S$X_te, G$config)
+  baseline_ll <- logL_TRUE_dim(M_TRUE, S$X_te)
+})[["elapsed"]]
 
-t_true  <- system.time(logL_TRUE_dim(M_TRUE, S$X_te))["elapsed"]
-t_trtf <- system.time(logL_TRTF_dim(M_TRTF, S$X_te))["elapsed"]
-t_ks   <- system.time(logL_KS_dim(M_KS,   S$X_te))["elapsed"]
-t_ttm  <- system.time(logL_TTM_dim(M_TTM, S$X_te))["elapsed"]
+t_trtf <- system.time({
+  M_TRTF <- fit_TRTF(S$X_tr, S$X_te, G$config)
+  trtf_ll <- logL_TRTF_dim(M_TRTF, S$X_te)
+})[["elapsed"]]
 
-baseline_ll <- logL_TRUE_dim(M_TRUE, S$X_te)
-trtf_ll     <- logL_TRTF_dim(M_TRTF, S$X_te)
-ks_ll       <- logL_KS_dim(M_KS, S$X_te)
-ttm_ll      <- logL_TTM_dim(M_TTM, S$X_te)
+t_ks <- system.time({
+  M_KS <- fit_KS(S$X_tr, S$X_te, G$config)
+  ks_ll <- logL_KS_dim(M_KS, S$X_te)
+})[["elapsed"]]
+
+t_ttm <- system.time({
+  M_TTM <- fit_TTM(S$X_tr, S$X_te)
+  ttm_ll <- logL_TTM_dim(M_TTM, S$X_te)
+})[["elapsed"]]
 
 tab_normal <- data.frame(
   dim = as.character(seq_along(G$config)),
@@ -56,15 +61,25 @@ X_te_p <- S$X_te[, c(2,1,3,4)[seq_len(ncol(S$X_te))], drop = FALSE]
 colnames(X_tr_p) <- paste0("X", seq_len(ncol(X_tr_p)))
 colnames(X_te_p) <- paste0("X", seq_len(ncol(X_te_p)))
 
-M_TRUE_p <- fit_TRUE(X_tr_p, X_te_p, G$config)
-M_TRTF_p <- fit_TRTF(X_tr_p, X_te_p, G$config)
-M_KS_p   <- fit_KS(X_tr_p, X_te_p, G$config)
-M_TTM_p  <- fit_TTM(X_tr_p, X_te_p)
+t_true_p <- system.time({
+  M_TRUE_p <- fit_TRUE(X_tr_p, X_te_p, G$config)
+  baseline_ll_p <- logL_TRUE_dim(M_TRUE_p, X_te_p)
+})[["elapsed"]]
 
-baseline_ll_p <- logL_TRUE_dim(M_TRUE_p, X_te_p)
-trtf_ll_p     <- logL_TRTF_dim(M_TRTF_p, X_te_p)
-ks_ll_p       <- logL_KS_dim(M_KS_p, X_te_p)
-ttm_ll_p      <- logL_TTM_dim(M_TTM_p, X_te_p)
+t_trtf_p <- system.time({
+  M_TRTF_p <- fit_TRTF(X_tr_p, X_te_p, G$config)
+  trtf_ll_p <- logL_TRTF_dim(M_TRTF_p, X_te_p)
+})[["elapsed"]]
+
+t_ks_p <- system.time({
+  M_KS_p <- fit_KS(X_tr_p, X_te_p, G$config)
+  ks_ll_p <- logL_KS_dim(M_KS_p, X_te_p)
+})[["elapsed"]]
+
+t_ttm_p <- system.time({
+  M_TTM_p <- fit_TTM(X_tr_p, X_te_p)
+  ttm_ll_p <- logL_TTM_dim(M_TTM_p, X_te_p)
+})[["elapsed"]]
 
 tab_perm <- data.frame(
   dim = as.character(seq_along(G$config)),
@@ -89,10 +104,10 @@ for (nm in names(tab_perm)) {
 tab_perm <- rbind(tab_perm, as.data.frame(sum_row, stringsAsFactors = FALSE))
 
 vec_normal <- c(true = t_true, trtf = t_trtf, ks = t_ks, ttm = t_ttm)
+vec_perm   <- c(true = t_true_p, trtf = t_trtf_p, ks = t_ks_p, ttm = t_ttm_p)
 
 kbl_obj <- combine_logL_tables(tab_normal, tab_perm,
-                              M_TRUE_p, M_TRTF_p, M_KS_p, M_TTM_p, X_te_p,
-                              vec_normal)
+                              vec_normal, vec_perm)
 
 setwd(old_wd)
 
