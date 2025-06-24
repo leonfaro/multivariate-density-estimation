4f8fbea6da5a6ce431629fd7c94bbc46f948e8f1
Léon Kia Faro <leonkiafaro@gmail.com>
2025-06-24 12:45:41 +0200

    EDA-Plots modularisiert

---
+++ tests/testthat/test_plot_functions.R
@@ -0,0 +1,29 @@
+source("helper_config.R")
+source("../../EDA.R")
+source("../../models/trtf_model.R")
+source("../../models/ks_model.R")
+source("../../models/true_model.R")
+source("../../02_split.R")
+
+set.seed(5)
+prep <- prepare_data(20, config, c(3,2,1,4))
+mods_n <- fit_models(prep$S, config)
+mods_p <- fit_models(prep$S_perm, config)
+scat_n <- make_scatter_data(mods_n, prep$S)
+scat_p <- make_scatter_data(mods_p, prep$S_perm)
+scatter_data <- c(scat_n, setNames(scat_p, paste0(names(scat_p), "_p")))
+
+plots_sc <- plot_scatter(scatter_data)
+plots_par <- plot_parameters(prep$param_list)
+
+test_that("plot_scatter returns four recorded plots", {
+  expect_length(plots_sc, 4)
+  expect_true(all(vapply(plots_sc, inherits, logical(1), "recordedplot")))
+})
+
+test_that("plot_parameters returns recorded plots", {
+  expect_true(length(plots_par) >= 0)
+  if (length(plots_par) > 0) {
+    expect_true(all(vapply(plots_par, inherits, logical(1), "recordedplot")))
+  }
+})
