c6959baea63254fabcf7101bb92be3546146acbc
Léon Kia Faro <leonkiafaro@gmail.com>
2025-07-02 14:31:15 +0200

    TTM in Pipeline integriert

--- tests/testthat/test_calc_loglik_tables.R
+++ tests/testthat/test_calc_loglik_tables.R
@@ -3,18 +3,21 @@ source("../../04_evaluation.R")
 source("../../models/trtf_model.R")
 source("../../models/ks_model.R")
 source("../../models/true_model.R")
+source("../../models/ttm_base.R")
+source("../../models/ttm_marginal.R")
 
 set.seed(3)
 prep <- prepare_data(30, config)
 mods <- list(
   true = fit_TRUE(prep$S, config),
   trtf = fit_TRTF(prep$S, config),
-  ks   = fit_KS(prep$S, config)
+  ks   = fit_KS(prep$S, config),
+  ttm  = trainMarginalMap(prep$S)
 )
 tab <- calc_loglik_tables(mods, config, prep$S$X_te)
 
 test_that("calc_loglik_tables works", {
   expect_true(is.data.frame(tab))
   expect_equal(nrow(tab), length(config) + 1)
-  expect_true(all(grepl("±", tab$true)))
+  expect_true(all(grepl("±", tab$ttm)))
 })
--- tests/testthat/test_loglik_sd_format.R
+++ tests/testthat/test_loglik_sd_format.R
@@ -3,17 +3,20 @@ source("../../04_evaluation.R")
 source("../../models/trtf_model.R")
 source("../../models/ks_model.R")
 source("../../models/true_model.R")
+source("../../models/ttm_base.R")
+source("../../models/ttm_marginal.R")
 
 set.seed(7)
 prep <- prepare_data(30, config)
 mods <- list(
   true = fit_TRUE(prep$S, config),
   trtf = fit_TRTF(prep$S, config),
-  ks   = fit_KS(prep$S, config)
+  ks   = fit_KS(prep$S, config),
+  ttm  = trainMarginalMap(prep$S)
 )
 tab <- calc_loglik_tables(mods, config, prep$S$X_te)
 
 test_that("calc_loglik_tables returns strings with plusminus", {
   expect_true(is.data.frame(tab))
-  expect_true(all(grepl("±", tab$trtf)))
+  expect_true(all(grepl("±", tab$ttm)))
 })
