acc1efc693a23878fe129188439b10068ef46c31
Léon Kia Faro <leonkiafaro@gmail.com>
2025-07-09 08:11:17 +0200

    Implementiere MAF und passe Tests an

---
+++ tests/testthat/test_maf_fit.R
@@ -0,0 +1,12 @@
+source("../../models/MAF_5.R")
+test_that("MAF fits and predicts", {
+  set.seed(1)
+  S <- list(X_tr = matrix(rnorm(100*4), 100, 4),
+            X_te = matrix(rnorm(100*4), 100, 4))
+  config <- list()
+  model <- fit_MAF(S, config, n_epoch = 1)
+  lp    <- predict(model, S$X_te, "logdensity")
+  expect_equal(length(lp), 100)
+  expect_true(all(is.finite(lp)))
+})
+
--- tests/testthat/test_maf_placeholder.R
+++
@@ -1,5 +0,0 @@
-test_that("MAF placeholder throws", {
-  S <- list(X_tr = matrix(rnorm(100*4), 100, 4))
-  config <- list()
-  expect_error(fit_MAF(S, config))
-})
