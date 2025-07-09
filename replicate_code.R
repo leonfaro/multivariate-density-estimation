b88f5d51fa4dd02debc5524b278830b773e88dca
Léon Kia Faro <leonkiafaro@gmail.com>
2025-07-09 18:46:47 +0200

    Ausgabe von main.R und Ergebnistabelle in repliziertem Code

--- replicate_code.R
+++ replicate_code.R
@@ -6,7 +6,9 @@ extract_sources <- function(main_file = "main.R") {
   unique(src_files)
 }
 
-replicate_code_scripts <- function(main_file = "main.R", outfile = "replicated_code.txt") {
+replicate_code_scripts <- function(main_file = "main.R",
+                                   outfile = "replicated_code.txt",
+                                   env = parent.frame()) {
   src_files <- extract_sources(main_file)
   output_lines <- character()
   for (f in src_files) {
@@ -15,9 +17,36 @@ replicate_code_scripts <- function(main_file = "main.R", outfile = "replicated_c
       lines <- sub("#.*$", "", lines)
       lines <- trimws(lines)
       lines <- lines[nchar(lines) > 0]
-      output_lines <- c(output_lines, paste0("### Begin ", f, " ###"), lines, paste0("### End ", f, " ###"), "")
+      output_lines <- c(output_lines,
+                        paste0("### Begin ", f, " ###"),
+                        lines,
+                        paste0("### End ", f, " ###"),
+                        "")
     }
   }
+
+  ## append main file itself
+  if (file.exists(main_file)) {
+    lines <- readLines(main_file, warn = FALSE)
+    lines <- sub("#.*$", "", lines)
+    lines <- trimws(lines)
+    lines <- lines[nchar(lines) > 0]
+    output_lines <- c(output_lines,
+                      paste0("### Begin ", main_file, " ###"),
+                      lines,
+                      paste0("### End ", main_file, " ###"),
+                      "")
+  }
+
+  if (exists("results_table", envir = env)) {
+    tab <- get("results_table", envir = env)
+    tab_lines <- capture.output(print(tab))
+    output_lines <- c(output_lines,
+                      "### Final results table ###",
+                      tab_lines,
+                      "")
+  }
+
   writeLines(output_lines, outfile)
 }
 
