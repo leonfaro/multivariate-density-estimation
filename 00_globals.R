62ba729c9eed239a3575afec967a1ef45b467c24
Léon Kia Faro <leonkiafaro@gmail.com>
2025-07-09 09:14:08 +0200

    Update 00_globals.R

--- 00_globals.R
+++ 00_globals.R
@@ -7,13 +7,12 @@ for (p in pkgs) {
 }
 
 if (!reticulate::py_module_available("tensorflow_probability")) {
-  message("→ installiere TensorFlow 2.19 + TFP 0.23 (≈ kompatibel zu Py 3.12)…")
   tensorflow::install_tensorflow(
-    version         = "2.19.0",                      # läuft mit Python 3.12
-    extra_packages  = "tensorflow-probability==0.23.0",
-    pip_ignore_installed = TRUE)
+    version              = "2.15.0",
+    extra_packages       = "tensorflow-probability==0.22.0",
+    pip_ignore_installed = TRUE
+  )
 }
-
 
 library(parallel)
 if (!exists("NC")) NC <- detectCores()
