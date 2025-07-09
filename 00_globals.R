13a502ef3e513431ff8c49d91dbdaebd5a74b43f
Léon Kia Faro <leonkiafaro@gmail.com>
2025-07-09 07:44:00 +0200

    Update 00_globals.R

--- 00_globals.R
+++ 00_globals.R
@@ -1,4 +1,10 @@
+pkgs <- c("trtf", "dplyr", "tibble", "tidyr")
 
+for (p in pkgs) {
+  if (!requireNamespace(p, quietly = TRUE))
+    install.packages(p, repos = "https://cloud.r-project.org")
+  library(p, character.only = TRUE)
+}
 
 library(parallel)
 if (!exists("NC")) NC <- detectCores()
