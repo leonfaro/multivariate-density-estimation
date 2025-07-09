88f76e4f07524a0feb3da125cc3c44357e5993ae
Léon Kia Faro <leonkiafaro@gmail.com>
2025-07-09 07:56:22 +0200

    Platzhalter f\303\274r MAF-Modell und Abh\303\244ngigkeiten

---
+++ models/MAF_5.R
@@ -0,0 +1,9 @@
+#' Masked Autoregressive Flow (Gerüst)
+#' @keywords internal
+fit_MAF <- function(S, config,
+                    n_hidden = 100,
+                    n_flows  = 5,
+                    lr       = 1e-3,
+                    n_epoch  = 200) {
+  stop("MAF noch nicht implementiert – Prompt 2 ausführen!")
+}
