7079416056fe227ff24116e898d576360984a360
Léon Kia Faro <leonkiafaro@gmail.com>
2025-08-19 16:09:30 +0200

    Guards und deterministische Tests für Cross-term-Map

---
+++ tests/testthat/test_ttm_cross_term_repro.R
@@ -0,0 +1,17 @@
+source("helper_config.R")
+source("../../04_evaluation.R")
+
+set.seed(16)
+prep <- prepare_data(30, config)
+
+set.seed(101)
+fit1 <- trainCrossTermMap(prep$S)
+set.seed(101)
+fit2 <- trainCrossTermMap(prep$S)
+
+(test_that("trainCrossTermMap reproducible", {
+  expect_equal(fit1$NLL_test, fit2$NLL_test)
+  p1 <- predict(fit1$S, prep$S$X_te, "logdensity")
+  p2 <- predict(fit2$S, prep$S$X_te, "logdensity")
+  expect_equal(p1, p2, tolerance = 1e-12)
+}))
